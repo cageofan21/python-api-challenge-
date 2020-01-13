f10514a94ee6a46a5be9a69b39dfab3060feb7ca
Adib Khan <adib.cena@gmail.com>
2020-01-13 00:13:22 -0500

    Homework 06 Upload

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "018202ce994fe151514b7236daca8402"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyDr0eBNPCe710Mob5-Nwlszjy8C9HPR9ww"
+g_key = ""
